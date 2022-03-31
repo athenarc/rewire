dd7fef7199a3c22f61bc996627f7635d3cf3ff1a
gtsoulou <tsoulouhas@gmail.com>
2022-03-31 16:52:14 +0300

    Add files via upload

---
+++ smartsantander/charts.php
@@ -0,0 +1,170 @@
+<?php
+
+$urn_id = $_GET["urn_id"];
+if(isset($_GET["start"])){
+    $start = $_GET["start"];
+} else {
+    $start = 0;
+}
+if(isset($_GET["limit"])){
+    $limit = $_GET["limit"];
+} else {
+    $limit = 10000;
+}
+
+
+
+
+$servername = "justhost.gr";
+$username = "rewire@justsoft.";
+$password = "TyPLnF7cq15lN5c";
+$dbname = "justsoft_gr_justsoft_smartsantander";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+//error_log($servername.$username.$password.$dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, TIME_FORMAT(`timestamp`,'%H:%i') as mnts ,UNIX_TIMESTAMP(`timestamp`) as ut, `uom`, `phenomenon`, `value` FROM `measurements` WHERE `urn_id` = ? AND `phenomenon` != 'batteryLevel' LIMIT $start, $limit";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $urn_id);
+$stmt->execute();
+printf("%s<br>", $stmt->error);
+
+$result = $stmt->get_result();
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $unixtime_as_index = $row["ut"];
+        $minutes[$unixtime_as_index] = $row["mnts"];
+        $data[$row["phenomenon"]."(".$row["uom"].")"][$unixtime_as_index] = $row["value"];
+    }
+}
+$conn->close();
+
+$colors_array = ['green','pink','red','black','silver','brown','blue','yellow','orange','magenta'];
+
+$csv_filename = "csv_files/".$urn_id.".csv";
+$f = fopen($csv_filename, 'w');
+if ($f === false) {
+    die('Error opening the file ' . $csv_filename);
+}
+?>
+<html>
+<header>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</header>
+<body>
+<p><a href="<?php echo $csv_filename;?>">Download data in csv</a></p>
+<form action="charts.php" method="get">
+    Start: <input type="text" value="<?php echo $start;?>" name="start">
+    <br>
+    Limit:  <input type="text" value="<?php echo $limit;?>" name="limit">
+    <br>
+    <input type="hidden" name="urn_id" value="<?php echo $urn_id?>">
+    <input type="submit">
+</form>
+<div>
+    <canvas id="myChart"></canvas>
+</div>
+<script>
+
+    const labels = [<?php
+        foreach ($minutes as $label) {
+            echo "'$label',";
+        } ?>];
+
+    const data = {
+        labels: labels,
+        datasets: [
+            <?php
+            $count=0;
+            $csv_headers_array[0] = "time";
+
+            foreach ($data as $data_label => $data_values) {
+                $csv_headers_array[] = $data_label;
+                $csv_data_array[] = $data_values;
+                ?>
+            {
+                label: '<?php echo $data_label;?>',
+                backgroundColor: '<?php echo $colors_array[$count];?>',
+                borderColor: '<?php echo $colors_array[$count];?>',
+                data: [<?php echo implode(",",$data_values);?>],
+            },
+                <?php
+                $count++;
+            }
+            ?>
+
+        ]
+    };
+
+    const config = {
+        type: 'line',
+        data: data,
+        options: {
+            legend: {
+                onClick: function(e, legendItem) {
+                    var index = legendItem.datasetIndex;
+                    var ci = this.chart;
+                    var alreadyHidden = (ci.getDatasetMeta(index).hidden === null) ? false : ci.getDatasetMeta(index).hidden;
+
+                    ci.data.datasets.forEach(function(e, i) {
+                        var meta = ci.getDatasetMeta(i);
+
+                        if (i !== index) {
+                            if (!alreadyHidden) {
+                                meta.hidden = meta.hidden === null ? !meta.hidden : null;
+                            } else if (meta.hidden === null) {
+                                meta.hidden = true;
+                            }
+                        } else if (i === index) {
+                            meta.hidden = null;
+                        }
+                    });
+
+                    ci.update();
+                },
+            },
+        }
+    };
+    // === include 'setup' then 'config' above ===
+
+    const myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+    );
+</script>
+<?php
+//array_unshift($csv_data_array,array_keys($minutes));
+$timestamps = array_keys($minutes);
+$csv_data_array = flip($csv_data_array);
+//print_r($csv_data_array);
+//$csv_data[] = $csv_headers_array;
+//array_keys($minutes);
+//$csv_data_array[0] = $minutes;
+fputcsv($f, $csv_headers_array);
+$csv_count=0;
+foreach ($csv_data_array as $row) {
+    array_unshift($row,$timestamps[$csv_count]);
+    fputcsv($f, $row);
+    $csv_count++;
+}
+fclose($f);
+
+function flip($arr)
+{
+    $out = array();
+    foreach ($arr as $key => $subarr)
+    {
+        foreach ($subarr as $subkey => $subvalue)
+        {
+            $out[$subkey][$key] = $subvalue;
+        }
+    }
+    return $out;
+}
+?>
+</body>
+</html>
---
+++ smartsantander/index.php
@@ -0,0 +1,172 @@
+<?php
+ob_start("ob_gzhandler");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #dddddd;
+        }
+    </style>
+</head>
+<body>
+<?php
+
+require_once 'ARIMA/src/arimaModel.php';
+$servername = "justhost.gr";
+$username = "rewire@justsoft.";
+$password = "TyPLnF7cq15lN5c";
+$dbname = "justsoft_gr_justsoft_smartsantander";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+//error_log($servername.$username.$password.$dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if(isset($_POST["measurements"])) {
+	
+	$measurements_array = $_POST["measurements"];
+	$urn = $_POST["urn"];
+	$time = $_POST["timestamp"];
+	$location_array = $_POST["location"];
+	$coordinates = $location_array["coordinates"][0].",".$location_array["coordinates"][1];
+	$location_type = $location_array["type"];
+    $urn_id=0;
+	$sql0 = "SELECT id FROM `urn` WHERE `urn_data` = '$urn'";
+    $result = $conn->query($sql0);
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $urn_id = $row["id"];
+    } else {
+        $sql1 = "INSERT INTO `urn` (urn_data,location_type,coordinates ) VALUES ('$urn','$location_type','$coordinates')";
+        if ($conn->query($sql1) === TRUE) {
+            $urn_id = $conn->insert_id;
+        }
+    }
+    foreach ($measurements_array as $measurement) {
+        $uom = $measurement["uom"];
+        $phenomenon = $measurement["phenomenon"];
+        $value = $measurement["value"];
+        if ($urn_id > 0) {
+            $sql2 = "INSERT INTO `measurements` (urn_id,timestamp,coordinates,location_type,uom,phenomenon,value) VALUES('$urn_id','$time','$coordinates','$location_type','$uom','$phenomenon','$value')";
+            if (!$conn->query($sql2) === TRUE) {
+                error_log("Error:".$conn->error, TRUE);
+            }
+        } else {
+            error_log("Error with urn id", TRUE);
+        }
+    }
+
+
+
+} else {
+    $sql0 = "SELECT * FROM urn";
+    $stmt = $conn->prepare($sql0);
+    $stmt->execute();
+    $result0 = $stmt->get_result();
+    $count=1;
+    echo '<table><tr><th>A/A</th><th>ID</th><th>urn</th><th>most freq. delay</th><th>min delay</th><th>max delay</th><th>avg delay</th><th>ARIMA prediction</th></tr>';
+    if ($result0->num_rows > 0) {
+        while($row0 = $result0->fetch_assoc()) {
+            $urn_type = "";
+            $urn_id = $row0["id"];
+            $urn_data = $row0["urn_data"];
+            /*
+            $sql1 = "SELECT id FROM `measurements` WHERE `urn_id` = '$urn_id' AND phenomenon = 'mileage:total' LIMIT 1";
+            $result1 = $conn->query($sql1);
+            if ($result1->num_rows > 0) {
+                $urn_type = 'mobile sensor';
+            }
+*/
+            $sql2 = "SELECT DISTINCT UNIX_TIMESTAMP(`timestamp`) as ut FROM `measurements` WHERE `urn_id` = ? ORDER BY ut ASC ";
+            $stmt2 = $conn->prepare($sql2);
+            $stmt2->bind_param("i", $urn_id);
+            $stmt2->execute();
+            //printf("%s<br>", $stmt2->error);
+
+            $result2 = $stmt2->get_result();
+            $unixtime_array = array();
+            if ($result2->num_rows > 0) {
+                while($row = $result2->fetch_assoc()) {
+                    $unixtime_array[] = $row["ut"];
+                }
+            }
+            $diffs_array = create_interval_diffs($unixtime_array);
+            $arima_prediction_array = array();
+            if(count($diffs_array) > 50){
+                $arima_prediction_array = arimaModel::auto_arima($diffs_array);
+            }
+
+            //print_r($diffs_array);
+            //exit;
+            $count_values = array_count_values($diffs_array);
+            arsort($count_values);
+
+            echo "<tr><td>$count.</td> ";
+            echo "<td>[<a href='charts.php?urn_id=$urn_id' target='_blank'>$urn_id</a>]</td>";
+            echo "<td>$urn_data</td>";
+            echo "<td>".round(key($count_values)/60,2)." min</td>";
+            echo "<td>".round(min($diffs_array)/60,2)." min</td>";
+            echo "<td>".round(max($diffs_array)/60,2)." min</td>";
+            //echo "<td>-</td>";
+            echo "<td>".round( (array_sum($diffs_array) / count($diffs_array))/60,2 )." min</td>";
+            echo "<td>".$arima_prediction_array[0]."sec (".round(($arima_prediction_array[0])/60,2)." min)</td>";
+            //echo "Type: $urn_type";
+            echo "</tr>";
+/*
+            $sql = "SELECT id, TIME_FORMAT(`timestamp`,'%H:%i') as mnts ,UNIX_TIMESTAMP(`timestamp`) as ut, `uom`, `phenomenon`, `value` FROM `measurements` WHERE `urn_id` = ? ";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("i", $urn_id);
+            $stmt->execute();
+
+            $result = $stmt->get_result();
+            if ($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                    $unixtime_as_index = $row["ut"];
+                    $minutes[$unixtime_as_index] = $row["mnts"];
+                    $data[$row["phenomenon"]."(".$row["uom"].")"][$unixtime_as_index] = $row["value"];
+                }
+            }
+*/
+            $count++;
+//if($count==50) exit;
+        }
+    }
+    echo "</table>";
+
+    $conn->close();
+}
+
+function create_interval_diffs($unixtime_array) {
+    $diffs = array();
+    for($i=1;$i<count($unixtime_array);$i++){
+        $diff = $unixtime_array[$i] - $unixtime_array[$i-1];
+        if($diff > 0 && $diff < 3600){
+            $diffs[] =  $diff;
+        }
+    }
+    return $diffs;
+}
+
+
+
+?>
+</body>
+</html>
+<?php
+ob_end_flush();
